0bd83b83cfd5be5bcd23f5a92cc82f20021dac3f
wilson evangelista dos santos filho <wilson.evangelista22@gmail.com>
2025-12-15 21:25:19 -0300

    :wrench:chore:initial project configuration

---
+++ credito-api-management/src/main/java/com/wildev/creditoapimanagement/CreditoApiManagementApplication.java
@@ -0,0 +1,13 @@
+package com.wildev.creditoapimanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditoApiManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditoApiManagementApplication.class, args);
+	}
+
+}
