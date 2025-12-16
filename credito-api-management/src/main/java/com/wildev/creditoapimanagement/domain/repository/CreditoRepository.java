34e5980fee88060e77f5811dafb1ad3fd2e9f704
wilson evangelista dos santos filho <wilson.evangelista22@gmail.com>
2025-12-15 22:29:02 -0300

    :sparkles: feat:add entity and credit repository

--- credito-api-management/src/main/java/com/wildev/creditoapimanagement/domain/repository/CreditoRepository.java
+++ credito-api-management/src/main/java/com/wildev/creditoapimanagement/domain/repository/CreditoRepository.java
@@ -1,4 +1,15 @@
 package com.wildev.creditoapimanagement.domain.repository;
 
-public class CreditoRepository {
+import com.wildev.creditoapimanagement.domain.model.Credito;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CreditoRepository extends JpaRepository<Credito, Long> {
+
+    List<Credito> findByNumeroNfse(String numeroNfse);
+    Optional<Credito> findByNumeroCredito(String numeroCredito);
 }
