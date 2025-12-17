f27491efbbcd19927f90d11eea6c85aa8f313c43
wilson evangelista dos santos filho <wilson.evangelista22@gmail.com>
2025-12-16 21:57:39 -0300

    :sparkles: feat:add service credito

--- credito-api-management/src/main/java/com/wildev/creditoapimanagement/domain/service/CreditoService.java
+++ credito-api-management/src/main/java/com/wildev/creditoapimanagement/domain/service/CreditoService.java
@@ -1,4 +1,11 @@
 package com.wildev.creditoapimanagement.domain.service;
 
+import com.wildev.creditoapimanagement.domain.model.Credito;
+
+import java.util.List;
+
 public interface CreditoService {
+    List<Credito> buscarPorNfse(String numeroNfse);
+
+    Credito buscarPorNumeroCredito(String numeroCredito);
 }
--- credito-api-management/src/main/java/com/wildev/creditoapimanagement/domain/service/impl/CreditoServiceImpl.java
+++ credito-api-management/src/main/java/com/wildev/creditoapimanagement/domain/service/impl/CreditoServiceImpl.java
@@ -1,4 +1,26 @@
 package com.wildev.creditoapimanagement.domain.service.impl;
 
-public class CreditoServiceImpl {
+import com.wildev.creditoapimanagement.domain.model.Credito;
+import com.wildev.creditoapimanagement.domain.repository.CreditoRepository;
+import com.wildev.creditoapimanagement.domain.service.CreditoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class CreditoServiceImpl implements CreditoService {
+
+    private final CreditoRepository repository;
+
+    @Override
+    public List<Credito> buscarPorNfse(String numeroNfse) {
+        return repository.findByNumeroNfse(numeroNfse) ;
+    }
+
+    @Override
+    public Credito buscarPorNumeroCredito(String numeroCredito) {
+        return repository.findByNumeroCredito(numeroCredito).orElseThrow(null);
+    }
 }
