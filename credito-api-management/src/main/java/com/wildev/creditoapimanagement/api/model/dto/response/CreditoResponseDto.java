913324db174581a493e6c02370f9dc9f885f3242
wilson evangelista dos santos filho <wilson.evangelista22@gmail.com>
2025-12-16 22:04:45 -0300

    :sparkles: feat:add credito response dto

---
+++ credito-api-management/src/main/java/com/wildev/creditoapimanagement/api/model/dto/response/CreditoResponseDto.java
@@ -0,0 +1,27 @@
+package com.wildev.creditoapimanagement.api.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreditoResponseDto {
+
+    private Long id;
+    private String numeroCredito;
+    private String numeroNfse;
+    private LocalDate dataConstituicao;
+    private BigDecimal valorIssqn;
+    private String tipoCredito;
+    private boolean simplesNacional;
+    private BigDecimal aliquota;
+    private BigDecimal valorFaturado;
+    private BigDecimal valorDeducao;
+    private BigDecimal baseCalculo;
+
+}
